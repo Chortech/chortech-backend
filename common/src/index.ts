17934cebcb6182e19aaf6c6e2ace249afc59ce76
sinashk78 <sina.sh.k78@gmail.com>
2020-10-22 01:32:30 +0330

    Updated Common Module

--- common/src/index.ts
+++ common/src/index.ts
@@ -3,6 +3,8 @@ export * from "./validations/schema";
 export * from "./middlewares/validationHandler";
 
 // Errors
+export * from "./middlewares/errorHandler";
 export * from "./errors/badRequestError";
 export * from "./errors/validationError";
-export * from "./middlewares/errorHandler";
+export * from "./errors/notFoundError";
+export * from "./errors/resouceConflictError";
