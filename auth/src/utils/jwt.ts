2125dd040357f647f32d1468bc36435045f18593
sinashk78 <sina.sh.k78@gmail.com>
2020-10-24 02:19:20 +0330

    Implemented basics of JWT

--- auth/src/utils/jwt.ts
+++ auth/src/utils/jwt.ts
@@ -1,13 +1,55 @@
-import jwt from "jsonwebtoken";
+import jwt, { decode } from "jsonwebtoken";
+import util from "util";
+import mongoose from "mongoose";
+import fs from "fs";
+
+const private_key = fs.readFileSync("../keys/chortec.key", "utf-8");
+const public_key = fs.readFileSync("../keys/chortec.key.pub", "utf-8");
+
+const i = "Chortec"; // Issuer
+const a = "chortect.com"; // Audience
+
+// const signOptions = {
+//   issuer: i,
+//   subject: that is yet to be determined,
+//   audience: a,
+//   expiresIn: "1h",
+//   algorithm: "RS256",
+// };
 
 class JWT {
-  static create(
-    payload: any,
-    key: string
-  ): { access: string; refresh: string } {
-    return {
-      access: "",
-      refresh: "",
-    };
+  static generateToken(payload: any, subject: string): string {
+    jwt.sign(payload, private_key, {
+      expiresIn: "1m",
+    });
+    const token = jwt.sign(payload, private_key, {
+      issuer: i,
+      subject: subject,
+      audience: a,
+      expiresIn: "1h",
+      algorithm: "RS256",
+    });
+
+    return token;
+  }
+
+  static verify(token: string): string | object {
+    const decoded = jwt.verify(token, public_key, {
+      issuer: i,
+      audience: a,
+      algorithms: ["RS256"],
+    });
+    return decoded;
   }
 }
+
+// const token = JWT.generateToken(
+//   {
+//     id: new mongoose.Types.ObjectId().toHexString(),
+//     name: "myName",
+//   },
+//   "example@gmail.com"
+// );
+
+// console.log(token);
+// console.log(JWT.verify(token));
