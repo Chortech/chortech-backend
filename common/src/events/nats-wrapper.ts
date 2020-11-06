b6f71da309dd852aec856440e560bb43b288e1ae
sinashk78 <sina.sh.k78@gmail.com>
2020-11-06 13:47:59 +0330

    Updated Common Module

--- common/src/events/nats-wrapper.ts
+++ common/src/events/nats-wrapper.ts
@@ -10,16 +10,20 @@ class NatsWrapper {
   }
 
   connect(clusterId: string, clientId: string, url: string): Promise<void> {
-    this._client = nats.connect(clusterId, clientId, { url });
-
     return new Promise((res, rej) => {
-      this.client.on("connect", () => {
-        console.log("Connected to NATS");
-        res();
-      });
-      this.client.on("error", (err) => {
+      try {
+        this._client = nats.connect(clusterId, clientId, { url });
+
+        this.client.on("connect", () => {
+          console.log("Connected to NATS");
+          res();
+        });
+        this.client.on("error", (err) => {
+          rej(err);
+        });
+      } catch (err) {
         rej(err);
-      });
+      }
     });
   }
 }
