82fc8adc78c3c3de0578568cfb020b72f837a2b9
Nima AbolhassanBeigi <nima.abolhassanbeigi@gmail.com>
2020-11-27 15:16:52 +0330

    add some tests for remove other card route and fixed a little bug in the route

---
+++ user/src/routes/__tests__/remove-other-credit-card.test.ts
@@ -0,0 +1,136 @@
+import request from "supertest";
+import { app } from "../../app";
+import { users } from "../../test/setup";
+
+it('should remove a credit card from from other cards', async () => {
+  const { id, token } = await global.signin(
+    users[0].id,
+    users[0].email,
+    users[0].phone
+  );
+
+  const response = await request(app)
+    .post('/api/user/credit-card/create')
+    .set('Authorization', `Bearer ${token}`)
+    .send({ number: '1234567812345678', name: 'nima' })
+    .expect(201);
+
+   const creditCard = response.body;
+   
+   await request(app)
+    .post('/api/user/credit-card/other/add')
+    .set('Authorization', `Bearer ${token}`)
+    .send({ cardId: creditCard.id })
+    .expect(200)
+
+    await request(app)
+    .post('/api/user/credit-card/other/remove')
+    .set('Authorization', `Bearer ${token}`)
+    .send({ cardId: creditCard.id })
+    .expect(200)
+});
+
+
+it('should not remove a credit card from from my other cards without an auth token', async () => {
+    const { id, token } = await global.signin(
+      users[0].id,
+      users[0].email,
+      users[0].phone
+    );
+  
+    const response = await request(app)
+      .post('/api/user/credit-card/create')
+      .send({ number: '1234567812345678', name: 'nima' })
+      .expect(401);
+  
+     const creditCard = response.body;
+     
+     await request(app)
+      .post('/api/user/credit-card/other/add')
+      .send({ cardId: creditCard.id })
+      .expect(401)
+  
+      await request(app)
+      .post('/api/user/credit-card/other/remove')
+      .send({ cardId: creditCard.id })
+      .expect(401)
+});
+
+
+it('should not remove a credit card from my other cards with an invalid auth token', async () => {
+  const { id, token } = await global.signin(
+    users[0].id,
+    users[0].email,
+    users[0].phone
+  );
+
+  const response = await request(app)
+    .post('/api/user/credit-card/create')
+    .set('Authorization', `Bearer fdajlsfjasdf`)
+    .send({ number: '1234567812345678', name: 'nima' })
+    .expect(401);
+
+   const creditCard = response.body;
+   
+   await request(app)
+    .post('/api/user/credit-card/other/add')
+    .set('Authorization', `Bearer fdjlasjfdas`)
+    .send({ cardId: creditCard.id })
+    .expect(401)
+
+    await request(app)
+    .post('/api/user/credit-card/my/remove')
+    .set('Authorization', `Bearer fjldaksjdfls`)
+    .send({ cardId: creditCard.id })
+    .expect(401)
+});
+
+
+it('should not remove a credit card from my other cards if the card does not exist', async () => {
+    const { id, token } = await global.signin(
+      users[0].id,
+      users[0].email,
+      users[0].phone
+    )
+  
+    await request(app)
+      .post('/api/user/credit-card/other/remove')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ cardId: 'bullshitshit' })
+      .expect(404)
+});
+
+
+it('should remove a credit card from my other cards if it is not in my other cards', async () => {
+  const { id, token } = await global.signin(
+    users[0].id,
+    users[0].email,
+    users[0].phone
+  );
+
+  const response = await request(app)
+    .post('/api/user/credit-card/create')
+    .set('Authorization', `Bearer ${token}`)
+    .send({ number: '1234567812345678', name: 'nima' })
+    .expect(201);
+
+   const creditCard = response.body;
+   
+   await request(app)
+    .post('/api/user/credit-card/other/add')
+    .set('Authorization', `Bearer ${token}`)
+    .send({ cardId: creditCard.id })
+    .expect(200)
+
+    await request(app)
+      .post('/api/user/credit-card/other/remove')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ cardId: creditCard.id })
+      .expect(200)
+
+    await request(app)
+      .post('/api/user/credit-card/other/remove')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ cardId: creditCard.id })
+      .expect(409)
+});
