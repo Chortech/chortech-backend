8d22c3cbc69c8497cfa82c991d2df283ba01a058
Nima AbolhassanBeigi <nima.abolhassanbeigi@gmail.com>
2020-12-20 23:43:22 +0330

    add user created listener to group service

---
+++ group/src/listeners/user-created-listener.ts
@@ -0,0 +1,25 @@
+import { IUserCreated, Listener, Subjects } from "@chortec/common";
+import { Message } from "node-nats-streaming";
+import User from "../models/user";
+export class UserCreatedListener extends Listener<IUserCreated> {
+  subject: Subjects.UserCreated = Subjects.UserCreated;
+  queueName = "group-service";
+  async onMessage(data: IUserCreated["data"], done: Message) {
+    try {
+      const user = User.build({
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        phone: data.phone,
+        friends: [],
+        expenseCheck: false
+      });
+
+      await user.save();
+
+      done.ack();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
