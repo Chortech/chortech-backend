b00ec71533e244ef5a18a4d6f18f9e0ac35cc436
Nima AbolhassanBeigi <nima.abolhassanbeigi@gmail.com>
2020-12-21 20:10:12 +0330

    fixed delete group route

--- group/src/routes/delete-group.ts
+++ group/src/routes/delete-group.ts
@@ -1,35 +1,34 @@
-// import Router from 'express';
-// import { BadRequestError, UnauthorizedError, requireAuth, NotFoundError } from '@chortec/common';
-// import { validate } from '@chortec/common';
-// import Joi from 'joi';
-// import User from '../models/user';
-// import Group from '../models/group';
+import Router from 'express';
+import { BadRequestError, UnauthorizedError, requireAuth, NotFoundError } from '@chortec/common';
+import mongoose from 'mongoose';
+import Group from '../models/group';
 
 
-// const router = Router();
+const router = Router();
 
-// router.delete('/', requireAuth, async (req, res) => {
-//     if (!req.user) throw new BadRequestError('Invalid state!');
+router.delete('/', requireAuth, async (req, res) => {
+  if (!req.user) throw new BadRequestError('Invalid state!');
 
-//     const id = req.user;
+  // const id = req.user;
 
-//     const user = await User.findById(id);
-//     const group = await Group.findById(req.group?.id);
+  const user = mongoose.Types.ObjectId(req.user.id);
+  const group = await Group.findById(req.group?.id);
 
-//     if (!user)
-//         throw new BadRequestError('Invalid state!');
+  if (!user)
+      throw new BadRequestError('Invalid state!');
     
-//     if (!group)
-//         throw new NotFoundError(`No groups exist with the id ${req.group?.id}`);
+  if (!group)
+      throw new NotFoundError(`No groups exist with the id ${req.group?.id}`);
     
-//     if (group.creator != user)
-//         throw new BadRequestError('You are not the owner of this group!');
+  if (group.creator != user)
+      throw new BadRequestError('You are not the owner of this group!');
     
-//     await Group.deleteOne(group);
 
-//     res.status(200).send({
-//         message: 'Deleted the group successfully!'
-//     });
-// });
+  await Group.deleteOne(group);
 
-// export { router as deleteGroupRouter };
+  res.status(200).send({
+    message: 'Deleted the group successfully!'
+  });
+});
+
+export { router as deleteGroupRouter };
