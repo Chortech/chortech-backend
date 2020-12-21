7f09e381eb55bd08b5156ef22f4b1a327b2fe6b1
Nima AbolhassanBeigi <nima.abolhassanbeigi@gmail.com>
2020-12-21 22:39:15 +0330

    modify group model

--- group/src/models/group.ts
+++ group/src/models/group.ts
@@ -7,28 +7,33 @@ import mongoose, { Schema, Document } from 'mongoose';
  */
 
 
+interface IMember {
+  id: mongoose.Types.ObjectId;
+  expenseCheck: boolean;
+}
+
 interface IGroup {
-    name: string;
-    creator: mongoose.Types.ObjectId;
-    members?: mongoose.Types.ObjectId[];
+  name: string;
+  creator: IMember;
+  members?: IMember[];
 }
 
 type GroupDoc = IGroup & Document;
 
 interface GroupModel extends mongoose.Model<GroupDoc> {
-    build(group: IGroup): GroupDoc;
+  build(group: IGroup): GroupDoc;
 }
 
 const groupSchema = new Schema({
-    name: { type: String, required: true },
-    creator: { type: String, ref: 'User', required: true },
-    members: [{ type: String, ref: 'User' }]
+  name: { type: String, required: true },
+  creator: { type: Map, ref: 'User', required: true },
+  members: [{ type: Map }]
 });
 
 groupSchema.statics.build = (group: IGroup) => new Group(group);
 
 const Group = mongoose.model<GroupDoc, GroupModel>('Group', groupSchema);
 
-export { Group };
+export { Group, IMember };
 
 export default Group;
