51c8cb3da44db7b2b12274eaf725dccde9b70db6
Nima AbolhassanBeigi <nima.abolhassanbeigi@gmail.com>
2020-12-23 20:30:56 +0330

    add leave group router to app.ts group service

--- group/src/app.ts
+++ group/src/app.ts
@@ -12,7 +12,7 @@ import { createGroupRouter } from './routes/create-group';
 import { getGroupRouter } from './routes/get-group';
 import { deleteGroupRouter } from './routes/delete-group';
 import { addMembersToGroupRouter } from './routes/add-members';
-// import { leaveGroupRouter } from './routes/leave-group';
+import { leaveGroupRouter } from './routes/leave-group';
 
 // setting up express
 const app = express();
@@ -24,7 +24,7 @@ app.use('/api/group/', createGroupRouter);
 app.use('/api/group/:id', getGroupRouter);
 app.use('/api/group/:id', deleteGroupRouter);
 app.use('/api/group/:id', addMembersToGroupRouter);
-// app.use('/api/group/:id/leave', leaveGroupRouter);
+app.use('/api/group/:id/leave', leaveGroupRouter);
 
 // if any of the above route handlers failed to run we need to show a 404 status code
 app.get('*', (req, res) => {
