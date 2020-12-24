94e245a0870349dc45b8fda20d4eaa222eac2958
Nima AbolhassanBeigi <nima.abolhassanbeigi@gmail.com>
2020-12-24 21:01:28 +0330

    modify add my credit card router

--- user/src/routes/add-my-credit-card.ts
+++ user/src/routes/add-my-credit-card.ts
@@ -15,19 +15,29 @@ import User from '../models/user';
 const router = Router();
 
 const addMyCreditCardSchema = Joi.object({
-    cardId: Joi.string()
+    number: Joi.string(),
+    name: Joi.string()
 });
 
 router.post('/', requireAuth, validate(addMyCreditCardSchema), async(req, res) => {
     if (!req.user)
         throw new BadRequestError('Invalid State!');
 
-    const { cardId } = req.body;
+    const { number, name } = req.body;
+    let id;
     
-    if (!(await CreditCard.exists({ _id: cardId })))
-        throw new NotFoundError(`${cardId} does not exist!`);
+    if (!(await CreditCard.exists({ number: number }))) {
+        const creditCard = CreditCard.build({
+            number,
+            name
+        });
+
+        const { _id } = await creditCard.save();
+        id = _id;
+    } else {
+        id = await CreditCard.findOne({ number: number, name: name });
+    }
     
-    const id = mongoose.Types.ObjectId(cardId);
 
     const raw = await User.updateOne(
         {
@@ -38,9 +48,9 @@ router.post('/', requireAuth, validate(addMyCreditCardSchema), async(req, res) =
     );
 
     if (raw.n === 0)
-        throw new ResourceConflictError(`${cardId} is already in your cards list!`);
+        throw new ResourceConflictError(`Card with the number ${number} is already in your cards list!`);
     
-    const user = await User.findById(req.user.id);
+    const user = await User.findById(req.user.id).populate('myCreditCards').populate('otherCreditCards').populate('friends');
 
     res.status(200).json({ user });
 });
