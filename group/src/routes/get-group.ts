a4d1ea1456d058d870171e9224fb04b81a8f9b07
Nima AbolhassanBeigi <nima.abolhassanbeigi@gmail.com>
2020-12-25 18:24:26 +0330

    refactor get group router

--- group/src/routes/get-group.ts
+++ group/src/routes/get-group.ts
@@ -1,6 +1,7 @@
 import Router from 'express';
 import { BadRequestError, NotFoundError, requireAuth } from '@chortec/common';
 import Group from '../models/group';
+import mongoose from 'mongoose';
 
 
 const router = Router();
@@ -8,13 +9,17 @@ const router = Router();
 router.get('/', requireAuth, async (req, res) => {
     if (!req.user) throw new BadRequestError('Invalid state!');
     
-    const group = await Group.findById(req.group?.id);
+    const exists = await Group.exists({ _id: req.group?.id });
 
-    if (!group)
+    if (!exists)
         throw new NotFoundError(`No group exists with id ${req.group?.id}`);
 
-    if (!group.members?.includes(req.user.id))
+    if (!(await Group.find({ _id: req.group?.id, 
+        members: { $in: [mongoose.Types.ObjectId(req.user.id)] } })))
         throw new BadRequestError('You are not a member of this group!');
+
+    
+    const group = await Group.findById(req.group?.id).populate('members').populate('creator');
     
     res.status(200).json({ group });
 });
