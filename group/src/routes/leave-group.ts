c56aec61100ae71fc549d6e1638fb9ee4b563993
Nima AbolhassanBeigi <nima.abolhassanbeigi@gmail.com>
2020-12-28 00:15:54 +0330

    fixed a bug in the leave group router in group service

--- group/src/routes/leave-group.ts
+++ group/src/routes/leave-group.ts
@@ -9,32 +9,38 @@ const router = Router();
 router.delete('/', requireAuth, async (req, res) => {
     if (!req.user) throw new BadRequestError('Invalid State!');
 
-    const group = await Group.findById(req.group?.id);
+    const exists = await Group.exists({ _id: req.group?.id });
     const user = mongoose.Types.ObjectId(req.user.id);
     
-    if (!group)
+    if (!exists)
         throw new NotFoundError(`No groups exist with the id ${req.group?.id}`);
 
-    if (group.inActiveExpenses.includes(user))
+    if (await Group.exists({ _id: req.group?.id, members: { $size: 1 } }))
+        await Group.deleteOne({ _id: req.group?.id });
+    else if (await Group.exists({ _id: req.group?.id, inActiveExpenses: { $in: [user] } }))
         throw new BadRequestError('You cannot leave the group because you are a participant in an expense!');
-    
-    if (group.creator == user) {
-        if (!group.members || group.members?.length == 1) {
-            await Group.deleteOne(group);
-            res.status(200).send('You left the group successfully.');
-            return;
-        } else {
-            group.creator = group.members[0] != user ? group.members[0] : group.members[1];
-        }
-    }
-
-    const index = group.members ? group.members?.indexOf(user, 0) : -1;
-    if (index <= -1)
+    else if (await Group.exists({ _id: req.group?.id, members: { $nin: [user] } }))
         throw new BadRequestError('You are not a member of this group!');
+    else if (await Group.exists({ _id: req.group?.id, creator: user })) {
+        const group = await Group.findById(req.group?.id); 
 
-    group.members?.splice(index, 1);
+        if (!group)
+            throw new NotFoundError(`No groups with the id ${req.group?.id}`); 
+            
+        group.creator = group.members[0].toHexString() != req.user.id ? group.members[0] : group.members[1];
 
-    await group.save();
+        await group.save();
+    }
+    
+    const raw = await Group.updateOne(
+        {
+            _id: req.group?.id
+        }, 
+        { $pull: { members: user } }
+    );
+
+    if (raw.n === 0)
+        throw new BadRequestError('Something went wrong when you tried to leave!');
 
     res.status(200).send('You left the group successfully.');
 });
