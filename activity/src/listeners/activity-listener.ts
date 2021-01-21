7be399444fb9d4b76591d22962ab2db92003ddc1
Nima AbolhassanBeigi <nima.abolhassanbeigi@gmail.com>
2021-01-22 02:10:14 +0330

    generalize all the activity listeners into one

---
+++ activity/src/listeners/activity-listener.ts
@@ -0,0 +1,43 @@
+import { IData, IActivity, Listener, Subjects } from '@chortec/common';
+import { Message } from 'node-nats-streaming';
+import Activity from '../models/activity';
+
+
+export class ActivityListener extends Listener<IActivity> {
+  subject: Subjects.Activity = Subjects.Activity;
+  queueName = 'activity-service';
+  
+  async onMessage(data: IActivity['data'], done: Message) {
+    try {
+      if (Array.isArray(data)) {
+        for (let activity of data) {
+          const act = Activity.build({
+            subject: activity.subject,
+            object: activity.object,
+            parent: activity.parent,
+            action: activity.action,
+            involved: activity.involved,
+            data: activity.data
+          });
+
+          await act.save();
+        }
+      } else {
+        const act = Activity.build({
+          subject: data.subject,
+          object: data.object,
+          parent: data.parent,
+          action: data.action,
+          involved: data.involved,
+          data: data.data
+        });
+
+        await act.save();
+      }
+
+      done.ack();
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
