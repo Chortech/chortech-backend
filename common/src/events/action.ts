1afa70364e90dcf3ef5ab51c638b4fcaeab4a8b6
Nima AbolhassanBeigi <nima.abolhassanbeigi@gmail.com>
2021-01-21 17:58:03 +0330

    add action for activity in common module

---
+++ common/src/events/action.ts
@@ -0,0 +1,10 @@
+export enum Action {
+  Created = 'CREATED',
+  Deleted = 'DELETED',
+  Updated = 'UPDATED',
+  Added = 'ADDED',
+  Removed = 'REMOVED',
+  Left = 'LEFT',
+  Paid = 'PAID',
+  Commented = 'COMMENTED'
+}
