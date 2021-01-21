de5e2dd301f7383c9a4ff05c6fa6292f43b61f60
Nima AbolhassanBeigi <nima.abolhassanbeigi@gmail.com>
2021-01-21 18:50:48 +0330

    add event subjects for activity service in common module

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -5,4 +5,15 @@ export enum Subjects {
   GroupCreated = "group:created",
   GroupUpdated = "group:updated",
   GroupDeleted = "group:deleted",
+  ActivityCommented = 'activity:commented',
+  ActivityGroupCreated = 'activity:group-created',
+  ActivityGroupDeleted = 'activity:group-deleted',
+  ActivityGroupAdded = 'activity:group-added',
+  ActivityGroupRemoved = 'activity:group-removed',
+  ActivityGroupUpdated = 'activity:group-updated',
+  ActivityGroupLeft = 'activity:group-left',
+  ActivityExpenseCreated = 'activity:expense-created',
+  ActivityExpensePaid = 'activity:expense-paid',
+  ActivityExpenseUpdated = 'activity:expense-updated',
+  ActivityExpenseDeleted = 'activity:expense-deleted'
 }
