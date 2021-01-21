fc8a7667a2708a9ff92212a6622018e8e972b5d2
Nima AbolhassanBeigi <nima.abolhassanbeigi@gmail.com>
2021-01-22 01:45:34 +0330

    modify activity events and delete a whole lot of duplicate code

--- common/src/events/activity-events.ts
+++ common/src/events/activity-events.ts
@@ -21,71 +21,7 @@ export enum Action {
   Commented = 'COMMENTED'
 }
 
-interface IActivityCommented {
-  subject: Subjects.ActivityCommented;
-  data: IData
+interface IActivity {
+  subject: Subjects.Activity;
+  data: IData | IData[];
 }
-
-interface IActivityGroupCreated {
-  subject: Subjects.ActivityGroupCreated;
-  data: IData
-}
-
-interface IActivityGroupDeleted {
-  subject: Subjects.ActivityGroupDeleted;
-  data: IData
-}
-
-interface IActivityGroupAdded {
-  subject: Subjects.ActivityGroupAdded;
-  data: IData
-}
-
-interface IActivityGroupRemoved {
-  subject: Subjects.ActivityGroupRemoved;
-  data: IData
-}
-
-interface IActivityGroupUpdated{
-  subject: Subjects.ActivityGroupUpdated;
-  data: IData
-}
-
-interface IActivityGroupLeft {
-  subject: Subjects.ActivityGroupLeft;
-  data: IData
-}
-
-interface IActivityExpenseCreated {
-  subject: Subjects.ActivityExpenseCreated;
-  data: IData
-}
-
-interface IActivityExpensePaid {
-  subject: Subjects.ActivityExpensePaid;
-  data: IData
-}
-
-interface IActivityExpenseUpdated {
-  subject: Subjects.ActivityExpenseUpdated;
-  data: IData
-}
-
-interface IActivityExpenseDeleted {
-  subject: Subjects.ActivityExpenseDeleted;
-  data: IData
-}
-
-export { 
-  IActivityCommented,
-  IActivityGroupCreated,
-  IActivityGroupDeleted,
-  IActivityGroupAdded,
-  IActivityGroupRemoved,
-  IActivityGroupUpdated,
-  IActivityGroupLeft,
-  IActivityExpenseCreated,
-  IActivityExpensePaid,
-  IActivityExpenseUpdated,
-  IActivityExpenseDeleted
-};
