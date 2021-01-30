c2fb4d6d8e123e92577ee9a4b253374d21be1ea1
Nima AbolhassanBeigi <nima.abolhassanbeigi@gmail.com>
2021-01-30 20:04:39 +0330

    add a few tests for activity route

---
+++ activity/src/routes/__test__/get-activity.test.ts
@@ -0,0 +1,43 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { users } from '../../test/setup';
+
+
+it('should get activities', async () => {
+  const { id, token } = await global.signin(
+    users[0].id,
+    users[0].email,
+    users[0].phone
+  );
+
+  await request(app)
+    .get('/api/activity')
+    .set('Authorization', `Bearer ${token}`)
+    .expect(200);
+});
+
+
+it('should not get activities without an auth token', async () => {
+  const { id, token } = await global.signin(
+    users[0].id,
+    users[0].email,
+    users[0].phone
+  );
+
+  await request(app)
+    .get('/api/activity')
+    .expect(200);
+});
+
+it('should not get activities with an invalid auth token', async () => {
+  const { id, token } = await global.signin(
+    users[0].id,
+    users[0].email,
+    users[0].phone
+  );
+
+  await request(app)
+    .get('/api/activity')
+    .set('Authorization', `fdlaksjfdlajfdalshfksaj`)
+    .expect(200);
+});
