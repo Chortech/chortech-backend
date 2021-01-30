f7d3827b0bec35a203b1ed658c5a55c14647d9e4
Nima AbolhassanBeigi <nima.abolhassanbeigi@gmail.com>
2021-01-30 21:37:35 +0330

    add secrets for group service

--- group/src/index.ts
+++ group/src/index.ts
@@ -4,9 +4,34 @@ import { natsWrapper } from "./utils/nats-wrapper";
 import { UserCreatedListener } from './listeners/user-created-listener';
 import { UserUpdatedListener } from './listeners/user-updated-listener';
 import { randomBytes } from "crypto";
+import fs from "fs";
+import path from "path";
 
 
 async function start() {
+
+    const secpath =
+    process.env.SECRETS ||
+    path.join(__dirname, "..", "..", "secrets", "group-service-secrets.json");
+
+    const secrets = JSON.parse(fs.readFileSync(secpath, "utf-8"));
+    process.env.STORAGE_REGION = secrets.STORAGE_REGION;
+    process.env.STORAGE_ENDPOINT = secrets.STORAGE_ENDPOINT;
+    process.env.STORAGE_ACCESSKEYID = secrets.STORAGE_ACCESSKEYID;
+    process.env.STORAGE_SECRET = secrets.STORAGE_SECRET;
+    process.env.STORAGE_BUCKET = secrets.STORAGE_BUCKET;
+
+    if (!process.env.STORAGE_REGION)
+        throw new Error("STORAGE_REGION must be defined!");
+    if (!process.env.STORAGE_ENDPOINT)
+        throw new Error("STORAGE_ENDPOINT must be defined!");
+    if (!process.env.STORAGE_ACCESSKEYID)
+        throw new Error("STORAGE_ACCESSKEYID must be defined!");
+    if (!process.env.STORAGE_SECRET)
+        throw new Error("STORAGE_SECRET must be defined!");
+    if (!process.env.STORAGE_BUCKET)
+        throw new Error("STORAGE_BUCKET must be defined!");
+
     const port = process.env.PORT || 3000;
     const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/group';
     const natsClusterId = process.env.NATS_CLUSTER_ID || "chortec";
